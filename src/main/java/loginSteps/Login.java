9b6742e015bf64c3abe77710c1eccf7ae1b46c9e
csenamac <csena.machado@outlook.com>
2022-07-15 15:27:18 -0300

    commit 6

--- src/main/java/loginSteps/Login.java
+++ src/main/java/loginSteps/Login.java
@@ -9,23 +9,24 @@ public class Login {
     WebDriver driver = Hooks.getDriver();
     @Given("Me  logueo con usuario y pass")
     public void meLogueoConUsuarioYPass(UserDataTable userData) {
-        driver.findElement(By.xpath("")).click();
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
         driver.findElement(By.xpath("")).sendKeys(userData.getUser());
         driver.findElement(By.xpath("")).sendKeys(userData.getPass());
     }
 
 
-    @Given("Lleno formulario de registro de usuario")
-    public void llenoFormularioDeRegistroDeUsuario(UserDataTable userData) {
-        driver.findElement(By.xpath("")).click();
-        driver.findElement(By.xpath("")).sendKeys(userData.getNombre());
-        driver.findElement(By.xpath("")).sendKeys(userData.getApellido());
-        driver.findElement(By.xpath("")).sendKeys(userData.getCorreo());
-        driver.findElement(By.xpath("")).sendKeys(userData.getCiudad());
-        driver.findElement(By.xpath("")).sendKeys(userData.getPais());
-        driver.findElement(By.xpath("")).sendKeys(userData.getPass());
-        driver.findElement(By.xpath("")).sendKeys(userData.getTelefono().toString());
-        driver.findElement(By.xpath("")).sendKeys(userData.getCodigoPostal().toString());
-        driver.findElement(By.xpath("")).click();
-    }
+
+//    @Given("Lleno formulario de registro de usuario")
+//    public void llenoFormularioDeRegistroDeUsuario(UserDataTable userData) {
+//        driver.findElement(By.xpath("")).click();
+//        driver.findElement(By.xpath("")).sendKeys(userData.getNombre());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getApellido());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getCorreo());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getCiudad());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getPais());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getPass());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getTelefono().toString());
+//        driver.findElement(By.xpath("")).sendKeys(userData.getCodigoPostal().toString());
+//        driver.findElement(By.xpath("")).click();
+//    }
 }
--- src/main/resources/features/Login.feature
+++ src/main/resources/features/Login.feature
@@ -6,26 +6,9 @@ Feature: Login en el sistema
   Scenario: Login usuario
     Given Me  logueo con usuario y pass
       | user | pass |
-      | abc  | 123  |
+      | probando.ando.csenamac@gmail.com  | probandoando2022  |
     When Doy click en el boton acceder
     Then Valido que el mensaje contenga
       | message              |
       | El usuario no existe |
 
-  Scenario: Registro usuario en la pagina
-    Given Lleno formulario de registro de usuario
-      | nombre | apellido | correo           | telefono       | password  | ciudad     | direccion | pais  | codigoPostal |
-      | abc    | 123      | pepito@gmail.com | 1111222333 | La Paloma | Keneddy SN | Uruguay   | 27001 | 27000        |
-    When Doy click en el boton acceder
-    Then Valido que el mensaje contenga
-      | message              |
-      | El usuario no existe |
-
-    Scenario: Seleccionar lista de productos
-      Given Selecciono la barra de busqueda
-      When Escribo nombre del producto "dresses"
-      And Selecciono todos los productos con Nombre "dresses"
-      |dress 1|
-      |dress 2|
-      |dress 3|
-      |dress 4|
